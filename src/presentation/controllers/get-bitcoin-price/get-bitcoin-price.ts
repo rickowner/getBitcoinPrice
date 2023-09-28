e7eaa5ea673f91f8be8397bb4f1ad2fe1ed23d15
rickowner <marhen09@gmail.com>
2023-09-28 08:19:31 -0300

    feat: ensure GetBitcoinPriceController returns 200 GetBitcoinPrice if valid currency are provided

--- src/presentation/controllers/get-bitcoin-price/get-bitcoin-price.ts
+++ src/presentation/controllers/get-bitcoin-price/get-bitcoin-price.ts
@@ -2,7 +2,7 @@ import { Controller, HttpRequest, HttpResponse } from 'presentation/protocols'
 
 export class GetBitcoinPriceController implements Controller {
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const retorno = {}
-    return await retorno as any
+    const retorno = await {}
+    return retorno as any
   }
 }
