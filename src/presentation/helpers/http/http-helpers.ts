00d7bbfead079ca67f2cb6ef744a92ef4e8b4cca
rickowner <marhen09@gmail.com>
2023-09-28 09:50:41 -0300

    feat: created Helpers for help call returns erros 401 and 500 or return 200 for a success

---
+++ src/presentation/helpers/http/http-helpers.ts
@@ -0,0 +1,18 @@
+import { ServerErrors, UnauthorizedErrors } from '../../erros'
+
+import { HttpResponse } from '../../protocols'
+
+export const unauthorized = (): HttpResponse => ({
+  statusCode: 401,
+  body: new UnauthorizedErrors()
+})
+
+export const ok = (data: any): HttpResponse => ({
+  statusCode: 200,
+  body: data
+})
+
+export const serverErrors = (error: Error): HttpResponse => ({
+  statusCode: 500,
+  body: new ServerErrors(error.stack)
+})
