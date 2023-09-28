2752915aa1b2b9f34102038dd634ce59f854fc6c
rickowner <marhen09@gmail.com>
2023-09-28 06:12:00 -0300

    chore: ensure config husky

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '<rootDir>/src/main/**'
+  ],
+  coverageDirectory: 'coverage',
+  collectCoverage: true,
+  testEnvironment: 'node',
+  preset: '@shelf/jest-mongodb',
+  transform: {
+    '^.+\\.(t|j)sx?$': 'ts-jest'
+  }
+}
