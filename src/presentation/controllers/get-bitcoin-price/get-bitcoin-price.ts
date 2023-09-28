950840388288c034fc8ca95a509443c869b7baf8
rickowner <marhen09@gmail.com>
2023-09-28 09:54:24 -0300

    feat: ensure GetBitcoinPriceController authenticate calls and returns success

--- src/presentation/controllers/get-bitcoin-price/get-bitcoin-price.ts
+++ src/presentation/controllers/get-bitcoin-price/get-bitcoin-price.ts
@@ -1,8 +1,19 @@
-import { Controller, HttpRequest, HttpResponse } from 'presentation/protocols'
+import { ok, serverErrors, unauthorized } from '../../helpers/http/http-helpers'
+import { Authentication, Controller, HttpRequest, HttpResponse } from './get-bitcoin-price.protocols'
 
 export class GetBitcoinPriceController implements Controller {
+  constructor (private readonly authentication: Authentication) {}
+
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const retorno = await {}
-    return retorno as any
+    try {
+      const { currency } = httpRequest.body
+      const accessToken = await this.authentication.auth({ currency })
+      if (!accessToken) {
+        return unauthorized()
+      }
+      return ok({ currency })
+    } catch (error) {
+      return serverErrors(new Error())
+    }
   }
 }
